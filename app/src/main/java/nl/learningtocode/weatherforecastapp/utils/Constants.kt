a750eb9fd361456ce027dda355e179de01212afd
Jokevtz <63862184+JokevtZ@users.noreply.github.com>
2022-02-01 18:10:51 +0100

    Update Constants.kt

--- app/src/main/java/nl/learningtocode/weatherforecastapp/utils/Constants.kt
+++ app/src/main/java/nl/learningtocode/weatherforecastapp/utils/Constants.kt
@@ -2,5 +2,5 @@ package nl.learningtocode.weatherforecastapp.utils
 
 object Constants {
     const val BASE_URL = "https://api.openweathermap.org/"
-    const val API_KEY = "6d160e2cdafd351aea7609bf93ac12ed"
+    const val API_KEY = ""
 }
