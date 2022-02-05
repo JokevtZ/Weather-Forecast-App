dae8e0190d77ac62c94b441a24604e6fc38d720a
jokelugtigheid <jokevtz@gmail.com>
2022-02-05 17:15:23 +0100

    added dropdown menu

--- app/src/main/java/nl/learningtocode/weatherforecastapp/widgets/WeatherAppBar.kt
+++ app/src/main/java/nl/learningtocode/weatherforecastapp/widgets/WeatherAppBar.kt
@@ -1,11 +1,14 @@
 package nl.learningtocode.weatherforecastapp.widgets
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.*
 import androidx.compose.material.icons.rounded.MoreVert
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -27,7 +30,16 @@ fun WeatherAppBar(
     elevation : Dp = 0.dp,
     navController : NavController,
     onAddActionClicked : () -> Unit = {},
-    onButtonClicked : () -> Unit = {}) {
+    onButtonClicked : () -> Unit = {})
+{
+
+    val showDialog = remember {
+        mutableStateOf(false)
+    }
+
+    if (showDialog.value){
+        ShowSettingsDropdownDialog(showDialog = showDialog, navController = navController)
+    }
 
     TopAppBar(
         title = {
@@ -46,7 +58,9 @@ fun WeatherAppBar(
                           Icon(imageVector = Icons.Default.Search,
                               contentDescription = "Search Icon" )
                       }
-                      IconButton(onClick = { /*TODO*/ }) {
+                      IconButton(onClick = {
+                          showDialog.value = true
+                      }) {
                           Icon(imageVector = Icons.Rounded.MoreVert,
                               contentDescription = "More Icon" )
                       }
@@ -68,4 +82,47 @@ fun WeatherAppBar(
         elevation = elevation)
 
 
+}
+
+@Composable
+fun ShowSettingsDropdownDialog(
+    showDialog: MutableState<Boolean>, 
+    navController: NavController) {
+
+    var expanded by remember { mutableStateOf(true) }
+    val items = listOf("Favorite", "Settings", "About")
+
+    Column(modifier = Modifier
+        .fillMaxWidth()
+        .wrapContentSize(Alignment.TopEnd)
+        .absolutePadding(top = 45.dp, right = 20.dp))
+    {
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false },
+            modifier = Modifier
+                .width(140.dp)
+                .background(Color.White))
+        {
+            items.forEachIndexed { index, text ->
+                DropdownMenuItem(onClick = {
+                    expanded = false
+                    showDialog.value = false
+                }) {
+                    Icon(
+                        imageVector = when (text) {
+                            "Favorite" -> Icons.Default.FavoriteBorder
+                            "Settings" -> Icons.Default.Settings
+                            else -> Icons.Default.Info
+                        },
+                        contentDescription = null,
+                        tint = Color.Gray)
+                    Text(text = text, modifier = Modifier.clickable
+                    {
+                    },
+                    fontWeight = FontWeight.W300)
+                }
+            }
+        }
+    }
 }
