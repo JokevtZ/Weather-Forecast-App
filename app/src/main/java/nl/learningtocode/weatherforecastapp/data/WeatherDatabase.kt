93fee2e23606a52e55a5a2a2a6c63f5822f21c72
jokelugtigheid <jokevtz@gmail.com>
2022-02-09 17:00:28 +0100

    bug fixing

--- app/src/main/java/nl/learningtocode/weatherforecastapp/data/WeatherDatabase.kt
+++ app/src/main/java/nl/learningtocode/weatherforecastapp/data/WeatherDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.RoomDatabase
 import nl.learningtocode.weatherforecastapp.model.Favorite
 import nl.learningtocode.weatherforecastapp.model.Unit
 
-@Database(entities = [Favorite::class, Unit::class], version = 2, exportSchema = false)
+@Database(entities = [Favorite::class, Unit::class], version = 3, exportSchema = false)
 abstract class WeatherDatabase : RoomDatabase() {
     abstract fun weatherDao() : WeatherDao
 }
